ee58b23abe21d77d505775968181e739dbfcd1e7
nikhilkni <nikhil.shakya2010@gmail.com>
2022-11-08 17:21:46 +0530

    calculateFibonacci

---
+++ fibonacci.cpp
@@ -0,0 +1,18 @@
+int fibonacci(int n)
+{
+    if(n==1)
+    return 0;
+    else if(n==2)
+    return 1;
+    else return fibonacci(n-1)+fibonacci(n-2);
+}
+
+
+int main()
+{
+    int n;
+    cout<<"Enter the number : ";
+    cin>>n;
+    cout<<fibonacci(n)<<endl;
+    return 0;
+}
